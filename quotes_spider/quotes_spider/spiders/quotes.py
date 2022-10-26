271f9fc13912278987394193e88b681cdd2b54c3
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-10-26 20:41:08 +0330

    update

--- quotes_spider/quotes_spider/spiders/quotes.py
+++ quotes_spider/quotes_spider/spiders/quotes.py
@@ -2,9 +2,12 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+  name = 'quotes'
+  allowed_domains = ['quotes.toscrape.com']
+  start_urls = ['http://quotes.toscrape.com/']  # no need for www
 
-    def parse(self, response):
-        pass
+  def parse(self, response):  # the name must be parse
+    h1_tag = response.xpath('//h1/a/text()').extract_first()
+    tags = response.xpath('//*[@class="tag-item"]/a/text()').extract()
+
+    yield {'H1 Tag': h1_tag, 'Tags': tags}
